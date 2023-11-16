5626e01f5f3d4650894ddc65e3964f944391a46c
unbearabletruth <antaarty@gmail.com>
2023-11-16 16:15:13 +0700

    Init js to ts for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home.jsx';
-import Post from './pages/Post';
+import Post from './pages/Post.jsx';
 
 function App() {
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { CommentContextProvider } from './context/CommentContext';
+import { CommentContextProvider } from './context/CommentContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CommentContextProvider>
       <App />
